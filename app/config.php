74181fa535490058dafa0de5e8e3e08bdbe61084
Joseph Chereshnovsky <jchereshnovsky@cogniance.com>
2016-12-27 13:46:10 +0200

    codebase cleanup

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'host' => '127.0.0.1',
-	'username' => 'root',
-	'password' => '1234',
-	'dbname' => 'aaaaa'
-);
--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// init db connection
-$dbConfig = include(APPLICATION_PATH . '/config.php');
-$db = Zend_Db::factory('Mysqli', $dbConfig);
-
-// run controller code
-require_once APPLICATION_PATH . '/Api.php';
